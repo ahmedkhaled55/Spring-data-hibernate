c584389857d9aa34b6748532aaa89582bae2142b
Your Name <you@example.com>
2023-08-23 12:14:49 +0300

    create one session and session factory

--- src/com/springdata/hibernate/MainApp.java
+++ src/com/springdata/hibernate/MainApp.java
@@ -3,29 +3,51 @@ package com.springdata.hibernate;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.springdata.hibernate.model.client;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String url = "jdbc:mysql://localhost:3306/employe?allowPublicKeyRetrieval=true&useSSL=false ";
-		String username ="root";
-		String password ="ahmed";
+		
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")			
+				.addAnnotatedClass(client.class)
+				.buildSessionFactory();
 		
 		
+		Session session = factory.getCurrentSession();
 		
-		try{
-			
-			Connection connection = DriverManager.getConnection(
-					
-		url,username,password
-					);
-			System.out.println("connected ");
-			System.out.println(connection);
-		}
-		catch(Exception e) {
-			System.out.println(e.toString());
-		}
+		
 		
 	}
 
 }
+
+
+
+/*
+
+String url = "jdbc:mysql://localhost:3306/employe?allowPublicKeyRetrieval=true&useSSL=false ";
+String username ="root";
+String password ="ahmed";
+
+
+
+try{
+	
+	Connection connection = DriverManager.getConnection(
+			
+url,username,password
+			);
+	System.out.println("connected ");
+	System.out.println(connection);
+}
+catch(Exception e) {
+	System.out.println(e.toString());
+}
+*/
